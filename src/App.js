b1fc2fcb5e283bf5b396a6b4b7164ec4be9ed323
Duncan Bain <duncan@bain.io>
2020-10-11 21:46:17 +0100

    Added header and navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import GlobalStyle from './theme/globalStyle'
+import React from "react";
+import GlobalStyle from "./theme/globalStyle";
+import styled from "styled-components";
+import Header from "./Components/Header";
 
+
+const AppContainer = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   return (
     <>
-    <GlobalStyle></GlobalStyle>
-    <div className="container">
-    </div>
+      <GlobalStyle />
+      <Header />
+      <AppContainer>
+      </AppContainer>
     </>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 1rem 0 1rem;
+`;
+
+const NavItems = styled.ul`
+  display: flex;
+  flex-direction: row;
+`;
+
+const NavItem = styled.li`
+  padding: 5px;
+  list-style: none;
+`;
+
+const NavLink = styled.a`
+  font-size: 1rem;
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <h1>React Weather App</h1>
+      <NavItems>
+        <NavItem>
+          <NavLink href="https://duncanbain.dev">duncanbain.dev</NavLink>
+        </NavItem>
+        <NavItem>
+          <a href="https://github.com/duncanjbain/">GitHub</a>
+        </NavItem>
+        <NavItem>
+          <a href="https://twitter.com/duncanbain">Twitter</a>
+        </NavItem>
+      </NavItems>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
