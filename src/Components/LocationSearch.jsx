f32b8a87fe703ae342141ba7613c9b1fe022bc99
Duncan Bain <duncan@bain.io>
2020-10-15 20:00:45 +0100

    Add get location button and icon to search

--- src/Components/LocationSearch.jsx
+++ src/Components/LocationSearch.jsx
@@ -1,43 +1,68 @@
 import React from "react";
 import styled from "styled-components";
-import {ReactComponent as SearchIconSvg} from "../assets/svg/search.svg"
-
+import { ReactComponent as SearchIconSvg } from "../assets/svg/search.svg";
+import { ReactComponent as PointerIconSvg } from "../assets/svg/pointer.svg";
 
 const SearchContainer = styled.div`
-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
-margin-bottom: 1.4rem;
-border-radius: 26px;
-display: flex;
-align-items: center;
-height: 3.25rem;
-position: relative;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+  margin-bottom: 1.4rem;
+  border-radius: 26px;
+  display: flex;
+  align-items: center;
+  height: 3.25rem;
+  position: relative;
 `;
 
 const LocationInput = styled.input`
-flex: 1;
-margin-left: 2rem;
-margin-right: 2rem;
-height: 3rem;
-border: none;
-font-size: 1.125rem;
-&:focus {
-  outline: none;
-}
+  flex: 1;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  height: 3rem;
+  border: none;
+  font-size: 1.125rem;
+  &:focus {
+    outline: none;
+  }
 `;
 
 const SearchIcon = styled(SearchIconSvg)`
-margin-left: 2rem;
-height: 2rem;
-width: auto;
+  margin-left: 2rem;
+  height: 2rem;
+  width: auto;
 `;
 
-const LocationSearch = ({locationSearchInput, updateLocationInput, clearLocationInput}) => {
-    return (
-        <SearchContainer>
-        <SearchIcon />
-        <LocationInput onChange={updateLocationInput} value={locationSearchInput} placeholder="Enter your location..." />
-        </SearchContainer>
-    )
-}
+const PointerIcon = styled(PointerIconSvg)`
+  margin-right: 2rem;
+  height: 2rem;
+  width: auto;
+`;
+
+const CurrentLocatonButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+  height: 100%;
+  display: contents;
+`;
+
+const LocationSearch = ({
+  locationSearchInput,
+  updateLocationInput,
+  clearLocationInput,
+}) => {
+  return (
+    <SearchContainer>
+      <SearchIcon />
+      <LocationInput
+        onChange={updateLocationInput}
+        value={locationSearchInput}
+        placeholder="Enter your location..."
+      />
+      <CurrentLocatonButton>
+        <PointerIcon />
+      </CurrentLocatonButton>
+    </SearchContainer>
+  );
+};
 
 export default LocationSearch;
