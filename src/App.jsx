2202aed5dcf9a6fd9276a162e132f2b28e853534
Duncan Bain <duncan@bain.io>
2020-10-18 11:53:19 +0100

    rename components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from "./components/node_modules/react";
 import staticWeather from "./json/staticLondon.json";
 import GlobalStyle from "./theme/globalStyle";
-import styled from "styled-components";
-import Header from "./Components/Header";
-import LocationSearch from "./Components/LocationSearch";
-import CurrentWeather from "./Components/CurrentWeather";
+import styled from "./components/node_modules/styled-components";
+import Header from "./components/Header";
+import LocationSearch from "./components/LocationSearch";
+import CurrentWeather from "./components/CurrentWeather";
 
 const AppContainer = styled.div``;
 
--- src/theme/globalStyle.jsx
+++ src/theme/globalStyle.jsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from '../components/node_modules/styled-components';
  
 const GlobalStyle = createGlobalStyle`
 *,
